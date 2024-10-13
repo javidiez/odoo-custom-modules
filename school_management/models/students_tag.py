7e63e3d26633383660f74327d0f56d8494a40c45
Javier Diez <javierdiez92@gmail.com>
2024-10-13 20:29:13 +0200

    MDF module name

---
+++ school_management/models/students_tag.py
@@ -0,0 +1,9 @@
+from odoo import models, fields, api
+
+class StudentTag(models.Model):
+    _name = "school.tag"
+    _description = "Etiquetas para alumnos"
+
+    name = fields.Char(string="Etiquetas", required=True)
+    description = fields.Text(string="Descripción")
+    color = fields.Integer(string="Color")
