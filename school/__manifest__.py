881a2edbc4e4a647b7083c728879821a951807fd
Javier Diez <javierdiez92@gmail.com>
2024-10-13 21:19:53 +0200

    MDF __manifest__

--- school/__manifest__.py
+++ school/__manifest__.py
@@ -9,7 +9,7 @@ Long description of module's purpose
     """,
 
     'author': "Javier Diez",
-    'website': "https://www.javierdiez.netlify.app",
+    'website': "https://javierdiez.netlify.app",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
--- school_management/models/students_tag.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-class StudentTag(models.Model):
-    _name = "school.tag"
-    _description = "Etiquetas para alumnos"
-
-    name = fields.Char(string="Tags", required=True)
-    description = fields.Text(string="Description")
-    color = fields.Integer(string="Color")
