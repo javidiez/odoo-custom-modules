fe0a870e3496292903343f35f7f2c100b2b77527
Javier Diez <javierdiez92@gmail.com>
2024-10-15 13:39:58 +0200

    Delete library_books_management directory

--- library_books_management/models/tags.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-class Tag(models.Model):
-    _name = "book.tag"
-    _description = "Book tag table"
-
-    name = fields.Char(string="Name")
-    description = fields.Text(string="Description")
-    color = fields.Integer(string="Color")
