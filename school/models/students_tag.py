cc2d337101708907a253bfcd40b43a767169b797
Javier Diez <javierdiez92@gmail.com>
2024-10-17 11:22:36 +0200

    Delete school directory

--- school/models/students_tag.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-class StudentTag(models.Model):
-    _name = "school.tag"
-    _description = "Etiquetas para alumnos"
-
-    name = fields.Char(string="Etiquetas", required=True)
-    description = fields.Text(string="Descripción")
-    color = fields.Integer(string="Color")
