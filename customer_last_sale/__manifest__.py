955d8e45eb052998fe77432c695ec08a82881523
Javier Diez <javierdiez92@gmail.com>
2024-10-20 13:32:17 +0200

    ADD module customer_last_sale

--- customer_last_sale/__manifest__.py
+++ customer_last_sale/__manifest__.py
@@ -1,34 +1,34 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "customer_last_sale",
+    'name': "Customer last sale",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Show the customer last sale date and the sale order",
 
     'description': """
-Long description of module's purpose
+Show the customer last sale date and the related sale order
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Javier Diez",
+    'website': "https://javierdiez.netlify.app/",
+    'license': 'AGPL-3',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Sale',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'sale'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/customer_last_sale_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    "images": ['static/images/banner.png', 'static/description/icon.png'],
 }
-
