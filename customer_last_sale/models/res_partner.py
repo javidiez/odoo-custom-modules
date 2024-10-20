48008b759e6465a2c51e3a7b86325ac2e00fc1c7
Javier Diez <javierdiez92@gmail.com>
2024-10-20 15:59:30 +0200

    MDF field string Last sale order

--- customer_last_sale/models/res_partner.py
+++ customer_last_sale/models/res_partner.py
@@ -6,4 +6,4 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     last_sale_order_date = fields.Datetime(string="Last sale order date", readonly=True)
-    sale_order_id = fields.Many2one('sale.order', string="Sale order", readonly=True)
+    sale_order_id = fields.Many2one('sale.order', string="Last sale order", readonly=True)
