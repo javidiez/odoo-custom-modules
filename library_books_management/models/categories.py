61ee4e04fca776098dc5047d225b2e1bd62c4ec7
Javier Diez <javierdiez92@gmail.com>
2024-12-16 18:19:22 +0100

    RMV modules from 17.0

--- library_books_management/models/categories.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-class Category(models.Model):
-    _name = "book.category"
-    _description = "Book category table"
-    
-    name = fields.Char(string="Category")
-    description = fields.Text(string="Description")
-    color = fields.Integer(string="Color")
